05a8df4f2744be76b87bb2b24e654c5abf838bca
wamozo cosmas <wcosmas75@gmail.com>
2023-06-06 23:23:12 +0300

    Setup Frontend and Chakra UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+// layouts and pages
+import RootLayout from './layouts/RootLayout';
+import Home from './pages/Home';
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
